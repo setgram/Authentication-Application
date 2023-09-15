5073adf1a39c981b60eeaf849b0827631eaabbd9
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-09-15 21:13:02 +0800

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">
+        Welcome to my Auth App!
+      </h1>
+      <p className="mb-4 text-slate-800">
+        {" "}
+        This is a full-stack web application built with the MERN (MongoDB,
+        Express, React, NodeJS) stack. It includes authentication features that
+        allow users to sign up, log in, and provides access to protected routes
+        only for authenticated users.
+      </p>
+    </div>
+  );
 }
