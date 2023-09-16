013184e9dbe6d7c2f6ac666ca1c5b704a36cbe5f
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-09-16 16:26:11 +0800

    bug fixed.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path"; // Import the 'path' module
 
 dotenv.config();
 
